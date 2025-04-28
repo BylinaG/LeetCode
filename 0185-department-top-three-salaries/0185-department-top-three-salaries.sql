f97c91041d0d3cedbef7a1818ba527e446c85da6
Grzegorz Bylina <87571579+BylinaG@users.noreply.github.com>
2025-04-28 14:16:06 +0200

    Time: 1129 ms (60.22%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -6,4 +6,4 @@ on e.departmentId = d.id
 where e.id in(select id 
 from (select *, dense_rank() over(partition by departmentId order by salary desc) as _rank
 from Employee e) t
-where _rank in(1,2,3))
+where _rank <= 3)
